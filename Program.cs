595df9eb91dd189db587b881e12e7f40fdd03ae3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-06 19:37:41 -0300

    atividade finalizada

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sair = "tchau";
+        string p = "";
+
+        Console.WriteLine("- groot bot -");
+
+        while (p != sair)
+        {
+            Console.Write("pergunta: ");
+            p = Console.ReadLine();
+
+            if (p.Trim().ToLower() == sair)
+            {
+                Console.WriteLine("eu sou groot!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("resposta: eu sou groot.");
+                Console.ResetColor();
+            }
+        }
+
+   
+    }
+}
